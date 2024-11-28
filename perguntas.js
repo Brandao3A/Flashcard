2e1ef189bf895181c87a335bfe3daeebfc7f5d22
Brandao3A <00001107662904sp@al.educacao.sp.gov.br>
2024-11-28 02:01:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'GEOGRFIA',
+    'Qual maior pais com recurso?',
+    'Rússia'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o maior musculo do corpo humano? ',
+    'quadríceps'
+)
+
+criaCartao(
+    'matematica',
+    'Resultado dessa soma 7859+693÷2?',
+    '8.205,5'
+)
+
+criaCartao(
+    'Português',
+    'Quem foi a mulher do Zumbi do Palmares?',
+    'Dandara dos Palmares'
+)
+
+criaCartao(
+    'Historia',
+    'Quem descobriu o Brasil',
+    'Pedro Álvares Cabral'
+)
+
+criaCartao(
+    'Quimica',
+    'Numero atomico do Ferro',
+    '26'
+)
